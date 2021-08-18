940c70bbc1d9aced9ea68c484531faa579ea8a0d
GHIRARDI Patrick <82661732+GHIRARDIPatrick@users.noreply.github.com>
2021-08-18 13:29:05 +0200

    PRODUIT&PANIER-Question&Quantité OK

--- src/panier.js
+++ src/panier.js
@@ -2,36 +2,40 @@
 // PANIER - Controle Formulaire
 
 alert("JS PANIER - Ca fonctionne");
+var messageErreur;
 
 document.getElementById("panier-prenom").addEventListener("input", function() {
     var code = document.getElementById("panier-prenom").value;
-    if (!code.match(/^([a-zA-Z]+)$/)) {
-        document.getElementById("panier-prenom").style.backgroundColor = 'red';
+    if (!code.match(/^([a-zA-Z -]+)$/)) {
+        document.getElementById("panier-prenom").style.backgroundColor = 'salmon';
         document.getElementById("panier-validation").disabled = true;
+        messageErreur("Uniquement des lettres, ainsi que ESPACE et TIRET");
     } else {
-        document.getElementById("panier-prenom").style.backgroundColor = 'green';
+        document.getElementById("panier-prenom").style.backgroundColor = 'chartreuse';
         document.getElementById("panier-validation").disabled = false;
     }
 });
 
 document.getElementById("panier-nom").addEventListener("input", function() {
     var code = document.getElementById("panier-nom").value;
-    if (!code.match(/^([a-zA-Z]+)$/)) {
-        document.getElementById("panier-nom").style.backgroundColor = 'red';
+    if (!code.match(/^([a-zA-Z -]+)$/)) {
+        document.getElementById("panier-nom").style.backgroundColor = 'salmon';
         document.getElementById("panier-validation").disabled = true;
+        messageErreur("Uniquement des lettres, ainsi que ESPACE et TIRET");
     } else {
-        document.getElementById("panier-nom").style.backgroundColor = 'green';
+        document.getElementById("panier-nom").style.backgroundColor = 'chartreuse';
         document.getElementById("panier-validation").disabled = false;
     }
 });
 
 document.getElementById("panier-adresse").addEventListener("input", function() {
     var code = document.getElementById("panier-adresse").value;
-    if (!code.length > 0) {
-        document.getElementById("panier-adresse").style.backgroundColor = 'red';
+    if (!code.match(/^([a-zA-Z0-9 -]+)$/)) {
+        document.getElementById("panier-adresse").style.backgroundColor = 'salmon';
         document.getElementById("panier-validation").disabled = true;
+        messageErreur("Uniquement des chiffres et des lettres, ainsi que ESPACE et TIRET");
     } else {
-        document.getElementById("panier-adresse").style.backgroundColor = 'green';
+        document.getElementById("panier-adresse").style.backgroundColor = 'chartreuse';
         document.getElementById("panier-validation").disabled = false;
     }
 });
@@ -39,21 +43,23 @@ document.getElementById("panier-adresse").addEventListener("input", function() {
 document.getElementById("panier-cpostal").addEventListener("input", function() {
     var code = document.getElementById("panier-cpostal").value;
     if (!code.match(/^([0-9]+)$/)) {
-        document.getElementById("panier-cpostal").style.backgroundColor = 'red';
+        document.getElementById("panier-cpostal").style.backgroundColor = 'salmon';
         document.getElementById("panier-validation").disabled = true;
+        messageErreur("Uniquement des chiffres");
     } else {
-        document.getElementById("panier-cpostal").style.backgroundColor = 'green';
+        document.getElementById("panier-cpostal").style.backgroundColor = 'chartreuse';
         document.getElementById("panier-validation").disabled = false;
     }
 });
 
 document.getElementById("panier-ville").addEventListener("input", function() {
     var code = document.getElementById("panier-ville").value;
-    if (!code.match(/^([a-zA-Z]+)$/)) {
-        document.getElementById("panier-ville").style.backgroundColor = 'red';
+    if (!code.match(/^([a-zA-Z -]+)$/)) {
+        document.getElementById("panier-ville").style.backgroundColor = 'salmon';
         document.getElementById("panier-validation").disabled = true;
+        messageErreur("Uniquement des lettres, ainsi que ESPACE et TIRET");
     } else {
-        document.getElementById("panier-ville").style.backgroundColor = 'green';
+        document.getElementById("panier-ville").style.backgroundColor = 'chartreuse';
         document.getElementById("panier-validation").disabled = false;
     }
 });
@@ -61,10 +67,32 @@ document.getElementById("panier-ville").addEventListener("input", function() {
 document.getElementById("panier-email").addEventListener("input", function() {
     var code = document.getElementById("panier-email").value;
     if (!code.length > 0) {
-        document.getElementById("panier-email").style.backgroundColor = 'red';
+        document.getElementById("panier-email").style.backgroundColor = 'salmon';
         document.getElementById("panier-validation").disabled = true;
     } else {
-        document.getElementById("panier-email").style.backgroundColor = 'green';
+        document.getElementById("panier-email").style.backgroundColor = 'chartreuse';
         document.getElementById("panier-validation").disabled = false;
     }
 });
+
+
+
+// PANIER - FENETRE MESSAGE ERREUR FORMULAIRE
+var modal = document.getElementById("myModal");
+var btn = document.getElementById("myBtn");
+var span = document.getElementsByClassName("close")[0];
+
+function messageErreur(messErr) {
+    modal.style.display = "block";
+    document.getElementById("panier-message").innerText = messErr;
+}
+
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
--- src/produit.js
+++ src/produit.js
@@ -1,16 +1,23 @@
 // PRODUIT - Quantité & Commande
 const produitQuantite = document.getElementById("produit-quantite");
+const produitCouleur = document.getElementById("produit-couleur-selection");
 let valeur = 0;
+
 
 
 alert("JS PRODUIT - Ca fonctionne");
 
+
+
 document
     .getElementById("produit-moins").addEventListener("click", function() {
-        if (produitQuantite < 1) {
+        if (--produitQuantite.value < 0) {
+            produitQuantite.value = 0;
+            document.getElementById("produit-quantite").innerText = (produitQuantite) + ' ';
             alert("Produit négatif");
+
         } else {
-            if (produitQuantite.value < 2) {
+            if (produitQuantite.value < 1) {
                 document.getElementById("produit-commande").disabled = true;
                 document.getElementById("produit-quantite").innerText = (--produitQuantite) + '';
 
@@ -22,15 +29,15 @@ document
     });
 
 
-document
-    .getElementById("produit-plus").addEventListener("click", function() {
-        document.getElementById("produit-quantite").innerText = (++produitQuantite.value) + '';
-        document.getElementById("produit-commande").disabled = false;
+document.getElementById("produit-plus").addEventListener("click", function() {
+    document.getElementById("produit-quantite").innerText = (++produitQuantite.value) + '';
+    document.getElementById("produit-commande").disabled = false;
+});
 
-    });
-
-document
-    .getElementById("produit-commande")
-    .addEventListener("click", function() {
+document.getElementById("produit-commande").addEventListener("click", function() {
+    if (produitCouleur.value || "") {
         alert("Mis en Panier");
-    });
+    } else {
+        alert("Merci de renseigner une couleur");
+    }
+});
