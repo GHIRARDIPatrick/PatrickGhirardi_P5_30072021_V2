bc036e45968e8afdfb4e37ccc493a3031a3477e2
GHIRARDI Patrick <82661732+GHIRARDIPatrick@users.noreply.github.com>
2021-08-18 20:04:20 +0200

    INDEX-JS GET Récupération liste produits

--- src/index.js
+++ src/index.js
@@ -1,21 +1,28 @@
 // ________________________________________________________________________________________________________________
 // INDEX - Affichage des oursons en liste
 
+
 alert("JS INDEX - Ca fonctionne");
 
-function askHello() {
-    fetch("http://localhost:3000/api/teddies/request?teddies")
-        .then(function(res) {
-            if (res.ok) {
-                return res.json();
-            }
-        })
-        .then(function(value) {
-            document
-                .getElementById("hello-result")
-                .innerText = value.queryString.greetings;
-        })
-        .catch(function(err) {
-            // Une erreur est survenue
-        });
-}
+fetch("http://127.0.0.1:3000/api/teddies")
+    .then(function(res) {
+        if (res.ok) {
+            alert("res.ok");
+            return res.json();
+        }
+    })
+    .then(function(resultat) {
+        alert("2ème Then");
+        var produitNombre = resultat.length;
+        const produit = resultat;
+        alert(resultat);
+        alert(produit[0].colors[1])
+        alert(produitNombre)
+        document
+            .getElementById("essai")
+            .innerText = produit[0].name;
+    })
+    .catch(function(err) {
+        alert("message erreur");
+        // Une erreur est survenue
+    });
