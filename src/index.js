ef80d11a6b4cec5989234f2cd78a6d6c612ddeb1
GHIRARDI Patrick <82661732+GHIRARDIPatrick@users.noreply.github.com>
2021-09-03 10:23:36 +0200

    BOOTSTRAP - Annulation MAJ Version

--- src/index.js
+++ src/index.js
@@ -19,7 +19,7 @@ fetch("http://127.0.0.1:3000/api/teddies")
         console.log("INDEX - Nb Articles téléchargés : " + produitNombre)
 
         for (let i = 0; i < produitNombre; i++) {
-            document.getElementById("JS").innerHTML += '<a onclick="myFunction(' + i + ')" href="produit.html" class="col-lg-4"><article><img src="' + produit[i].imageUrl + '" alt="Photo ' + produit[i].description + '"><h2 id>' + produit[i].name + '</h2><p>Prix : ' + produit[i].price / 100 + ' euros</p><span class="hidden">' + produit[i]._id + '"</span></article></a>';
+            document.getElementById("JS").innerHTML += '<a onclick="myFunction(' + i + ')" href="produit.html" class="col-lg-4"><article><img src="' + produit[i].imageUrl + '" alt="Photo ' + produit[i].description + '"><h2 id>' + produit[i].name + '</h2><p>Prix : ' + produit[i].price / 100 + ' euros</p><span class="hidden">' + produit[i]._id + '</span></article></a>';
 
             // document.getElementById("JS").innerHTML += '<a onclick="myFunction()" href="produit.html" class="col-lg-4"><article><img src="' + produit[i].imageUrl + '" alt="Photo ' + produit[i].description + '"><h2 id>' + produit[i].name + '</h2><p>Prix : ' + produit[i].price / 100 + ' euros</p><div class="form-group hidden"><label for="index-ref" class="control-label col-sm-2">Référence :</label><div class="col-lg-10"><input class="form-control-static" type="text" id="index-ref" name="index-ref" value="' + produit[i]._id + '" required></p></div></div></article></a>';
         }
